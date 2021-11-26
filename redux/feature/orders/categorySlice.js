067d31b76532fc788e4882e1a57c947a36cb1443
bahram <bahram.barati.job@gmail.com>
2021-11-26 09:46:50 +0330

    fetch fixed redux

---
+++ redux/feature/orders/categorySlice.js
@@ -0,0 +1,51 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = [
+
+];
+const ordersSlice = createSlice({
+    name: 'orders',
+    initialState,
+    reducers: {
+        orderAdded(state, action) {
+            const {id, name, amount, count} = action.payload;
+
+            const existOrder = state.find(order => order.id === id);
+
+            if (existOrder) {
+                existOrder.count = count + 1;
+            } else {
+                state.push(action.payload);
+            }
+        },
+        orderRemoved(state, action) {
+            const {id, count} = action.payload;
+
+            const existOrderRemove = state.find(order => order.id === id);
+
+            if (existOrderRemove) {
+                if (count === 1) {
+                    var index = state.indexOf(existOrderRemove);
+                    if (index > -1) {
+                        state.splice(index, 1);
+                    }
+                } else {
+                    existOrderRemove.count = count - 1;
+                }
+            }
+        },
+    },
+    prepare(id, name, image, amount, count) {
+        return {
+            payload: {
+                id,
+                name,
+                image,
+                amount,
+                count,
+            },
+        };
+    },
+});
+export const {orderAdded, orderRemoved} = ordersSlice.actions;
+export default ordersSlice.reducer;
